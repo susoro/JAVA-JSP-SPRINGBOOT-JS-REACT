d6aeffc9e82f7dd90e0bbbd4a783ae5a19b3b714
sugyeongsu <78544372+susoro@users.noreply.github.com>
2024-08-03 22:31:14 +0900

    Resolve merge conflicts

---
+++ Object-Oriented-Programming/src/org/studyeasy/Car.java
@@ -0,0 +1,9 @@
+package org.studyeasy;
+
+public class Car {
+    private String doors;
+    private String engine;
+    private String driver;
+    public int speed;
+
+}
---
+++ Object-Oriented-Programming/src/org/studyeasy/Main.java
@@ -0,0 +1,9 @@
+package org.studyeasy;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.speed = 120;
+        System.out.println(car.speed);
+    }
+}
